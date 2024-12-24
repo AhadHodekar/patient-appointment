76dfbed274c1bb85e1b213a8ad195b61becb4157
Ahad Hodekar <ahadparwezhodekar@gmail.com>
2024-12-24 15:40:46 +0000

    error handling added

--- server/middlewares/notFound.js
+++ server/middlewares/notFound.js
@@ -1,7 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 
 const notFound = (req, res) => {
-  res.status(StatusCodes.NOT_FOUND).send(`<h1>Route doesn't exist`);
+  res.status(StatusCodes.NOT_FOUND).send(`<h1>Route doesn't exist</h1>`);
 };
 
 export default notFound;
